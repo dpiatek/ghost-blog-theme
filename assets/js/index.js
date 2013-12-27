6b3064c6a92d88e6899bf3c88471ebb1e002f10f
Dominik Piatek <dominik.piatek@econsultancy.com>
2013-12-27 18:05:20 +0000

    Tweak fonts and colors

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,11 +8,6 @@
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
-
     });
 
 }(jQuery));
